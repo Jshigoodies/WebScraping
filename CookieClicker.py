929a4acfe49ee9949a955967cbfc52886b355c77
unknown <jshigoodies@gmail.com>
2020-12-24 18:25:54 -0800

    bot

--- CookieClicker.py
+++ CookieClicker.py
@@ -1,10 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
+from selenium.webdriver.common.action_chains import ActionChains
+
 import time
 
 PATH = "Driver\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
+
+action = ActionChains(driver)
+'''
 driver.maximize_window()
 driver.get("https://orteil.dashnet.org/cookieclicker/")
 
@@ -16,3 +21,4 @@ link = driver.find_element_by_id("bigCookie")
 
 while True:
     link.click()
+'''
---
+++ bot.py
@@ -0,0 +1,74 @@
+import discord
+from discord.ext import commands, tasks
+import random
+
+token = ""
+
+client = commands.Bot(command_prefix= ".")
+
+@client.event
+async def on_ready():
+    print("Bot is ready")
+
+@client.command(aliases=["h"])
+async def _help(ctx):
+    await ctx.send("h (help), ping, 8ball, join <- joins vc, leave <- leaves vc, clear (deletes the message above it), kick, ban, unban")
+
+status = ["Dead", "I feel the power of Anime", ":D :D :D"]
+
+@tasks.loop(seconds=20)
+async def change_status():
+    await client.change_presence(activity=discord.Game(choice(status)))
+
+@client.command()
+async def clear(ctx, amount=2):
+    await ctx.channel.purge(limit=amount)
+
+@client.command()
+async def ping(ctx):
+    await ctx.send(f"Pong! {round(client.latency * 1000)}ms")
+
+
+@client.command(aliases=["8ball", "eightball"])
+async def _8ball(ctx, *, question):
+    responses = ["no", "yes", "maybe"]
+    await ctx.send(f"Question: {question}\nAnswser: {random.choice(responses)}")
+
+
+@client.command()
+async def join(ctx):
+    channel = ctx.author.voice.channel
+    await channel.connect()
+
+
+@client.command()
+async def leave(ctx):
+    await ctx.voice_client.disconnect()
+
+
+@client.command()
+async def kick(ctx, member: discord.Member, *,
+               reason=None):  # the '*' is so you can make the reason a sentence instead of a single word after taking in the arguemnts of the command name and discord member
+    await member.kick(reason=reason)
+
+
+@client.command()
+async def ban(ctx, member: discord.Member, *, reason=None):
+    await member.ban(reason=reason)
+    await ctx.send(f"Banned {member.mention}")
+
+
+@client.command()
+async def unban(ctx, *, member):
+    banned_users = await ctx.guild.bans()
+    member_name, member_id = member.split("#")
+
+    for ban_entry in banned_users:
+        user = ban_entry.user
+
+        if (user.name, user.discriminator) == (member_name, member_id):
+            await ctx.guild.unban(user)
+            await ctx.send(f"Unbanned {user.name}#{user.discriminator}. Very sorry {user.mention}")
+            break
+
+client.run(token)
