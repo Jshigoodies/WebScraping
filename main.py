f37abc9ca960921457ea78127bea291335dec284
unknown <jshigoodies@gmail.com>
2020-12-28 21:53:22 -0800

    commit

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 import unittest
 from selenium import webdriver
 import page
+
+PATH = "Driver\chromedriver.exe"
+
+
+class PythonOrgSearch(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome(PATH)
+        self.driver.get("https://www.python.org/")
