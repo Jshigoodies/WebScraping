d9b8c441c0064c8128af8ac1c9edaffb0916437c
unknown <jshigoodies@gmail.com>
2021-01-05 22:14:32 -0800

    commit

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands, tasks
 import random
 
-token = "NzkxODE1ODM4OTIyNDQwNzI1.X-Up9w.GeRNP1yWfzeBSRj7lpqrzCd7cFw"
+token = "" # WebScraper Application Discord Bot's Token
 
 client = commands.Bot(command_prefix= ".")
 
