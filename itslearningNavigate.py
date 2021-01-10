17a03af4e67b38b6aa25ec616894f75f2166891c
unknown <jshigoodies@gmail.com>
2021-01-09 20:07:58 -0800

    commit

--- itslearningNavigate.py
+++ itslearningNavigate.py
@@ -1,7 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 
 PATH = "Driver\chromedriver.exe"
@@ -20,13 +22,19 @@ driver.implicitly_wait(5)
 
 # username
 username = input("[Navigator]$ Please Enter Student Email: ")
-userLoginInput = driver.find_element_by_id("i0116")
+try:
+    userLoginInput = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "i0116")))
+except Exception:
+    driver.quit()
 userLoginInput.send_keys(username)
 userLoginInput.send_keys(Keys.ENTER)
 
 # password
 password = input("[Navigator]$ Please Enter Password: ")
-userLoginInputPassword = driver.find_element_by_id("i0118")
+try:
+    userLoginInputPassword = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "i0118")))
+except Exception:
+    driver.quit()
 userLoginInputPassword.send_keys(password)
 userLoginInputPassword.send_keys(Keys.ENTER)
 
