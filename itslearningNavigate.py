00d03509a98f0ef2f37d2846323add9fef69e755
unknown <jshigoodies@gmail.com>
2021-01-09 20:13:00 -0800

    commit

--- itslearningNavigate.py
+++ itslearningNavigate.py
@@ -24,29 +24,14 @@ driver.implicitly_wait(5)
 username = input("[Navigator]$ Please Enter Student Email: ")
 try:
     userLoginInput = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "i0116")))
-except Exception:
-    driver.quit()
-userLoginInput.send_keys(username)
-userLoginInput.send_keys(Keys.ENTER)
+finally:
+    userLoginInput.send_keys(username)
+    userLoginInput.send_keys(Keys.ENTER)
 
 # password
 password = input("[Navigator]$ Please Enter Password: ")
 try:
     userLoginInputPassword = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "i0118")))
-except Exception:
-    driver.quit()
-userLoginInputPassword.send_keys(password)
-userLoginInputPassword.send_keys(Keys.ENTER)
-
-# I really need to implement this code. Right now I'm being so fking lazy about waiting for elements to load,
-# but realistically it shouldn't happen,
-# but it's not optimized the best way
-"""
-try:
-    main = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.ID, "id"))
-    )
-    print(main.text)
-except:
-    driver.quit() # probably won't do the quit() method
-"""
+finally:
+    userLoginInputPassword.send_keys(password)
+    userLoginInputPassword.send_keys(Keys.ENTER)
